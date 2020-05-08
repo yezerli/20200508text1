29b8e7fd78553f895095f6da4cec8565720b3c77
yezerli <1123603008@qq.com>
2020-05-08 14:55:50 +0800

    1

---
+++ 20200508text1/funtions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void on_mouse(int EVENT, int x, int y, int flags, void* userdata);
+int selectPolygon(cv::Mat srcMat, cv::Mat &dstMat);
+int mouseROI();
+int dftDemo();
+int ifftDemo();
